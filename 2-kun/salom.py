6b65868f2d7a032844da437981abcb00f00e42d1
Diyorbek <diyorbekaxmadjonov98@gamil.com>
2025-07-07 23:17:55 +0500

    add salom.py for bot functionality with /start command

---
+++ 2-kun/salom.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+import asyncio
+
+API_TOKEN = "8192874862:AAFp8EyJHxEl2TZzETXD2LrS3ffh3ecR_nE"
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+
+# /start komandasi
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    username = message.from_user.username
+    if username:
+        await message.answer(f"Salom, @{username}!")
+    else:
+        await message.answer("Salom, foydalanuvchi!")
+
+
+# Ishga tushirish
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
