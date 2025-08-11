265866f0f8481db2e443c0d5c386bc04c264e9ab
DIyorbek <diyorbekaxmadjonov98@gmail.com>
2025-08-11 19:30:04 +0500

    Add additional instructions and messages to test.py for bot development

--- 5-dars/buton/test.py
+++ 5-dars/buton/test.py
@@ -1,1 +1,7 @@
 print("Bot ishga tushdi")
+print("Bu yerda bot kodini yozing")
+print("Inline tugmalar, sana va vaqt, lokatsiya, bot haqida ma'lumot")
+# kabi funksiyalarni qo'shing")
+print("Asosiy menyu va callback handlerlarni yozing")
+print("Botni ishga tushirish uchun kerakli kutubxonalarni o'rnating")
+
